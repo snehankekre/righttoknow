09f1f6ed1ddde3fa1895dced9f834310defb6a5c
Snehan Kekre <snehan@minerva.kgi.edu>
2018-06-03 17:02:07 +0545

    refactor
    Updated paths

--- data/json_downloader.sh
+++ data/json_downloader.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-cd /Users/Minerva/Projects/righttoknow/data/json/
+cd /righttoknow/data/json/
 
 i=0
 suffix=.json
 while ((i <= 5000)); do
 	let i+=1
-	echo "Downloading page $i$suffix"
+	echo "Downloading file $i$suffix"
   curl -OJL "https://www.righttoknow.org.au/request/$i$suffix"
   echo "Downloaded page $i$suffix"
+  echo ""
 done
 
-echo "$i pages have been downloaded into /Users/Minerva/Projects/righttoknow/data/json/"
+echo "$i FOI requests downloaded in JSON to /righttoknow/data/json/"
